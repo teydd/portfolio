2e2d445e191745a41c88e66ee7fdd5b55bb6e873
teydd <chemosted@gmail.com>
2025-01-20 22:01:27 +0300

    whole

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/testimonials")
+def testimonial():
+    return render_template("testimonials.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+if __name__=="__main__":
+    app.run("0.0.0.0",8080,True)
